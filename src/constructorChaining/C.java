91b08d659b92fc8caec10fbc99136c6234e3046b
Parth Thakral <parth.thakral72@gmail.com>
2023-02-24 12:45:38 +0530

    super kwrd nad const chaining

--- src/constructorChaining/C.java
+++ src/constructorChaining/C.java
@@ -4,4 +4,12 @@ public class C extends B{
     C(){
         System.out.println("Constructor of class C");
     }
+
+    C(String c){
+        System.out.println("Cons with 1 param");
+    }
+
+    C(String a, String b){
+        System.out.println("Cons with params as and b");
+    }
 }
--- src/constructorChaining/D.java
+++ src/constructorChaining/D.java
@@ -2,6 +2,10 @@ package constructorChaining;
 
 public class D extends C{
     D(){
+        //super(); //explicitly call the cons of super/parent class .
+                    //its alredy calling the cost of parent class but if we want to call the custom constructor
+        super("Parth"); // this would call the custome constructor of parent class
+
         System.out.println("COnstructor of Class D");
     }
 }
